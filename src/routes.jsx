1e87b20ecfac59296f93bca77b1b635354b0d470
dmsergeev <hykkuu@gmail.com>
2017-04-24 21:54:54 -0500

    add course form + helper components

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,11 +4,13 @@ import App from './components/App.jsx';
 import HomePage from './components/home/HomePage.jsx';
 import AboutPage from './components/about/AboutPage.jsx';
 import CoursesPage from './components/courses/CoursesPage.jsx';
+import ManageCoursePage from './components/courses/ManageCoursePage.jsx';
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={HomePage} />
         <Route path="about" component={AboutPage} />
         <Route path="courses" component={CoursesPage} />
+        <Route path="course/:id" component={ManageCoursePage} />
     </Route>
 ) 
